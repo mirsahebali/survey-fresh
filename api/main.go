15f19b25d07ad1f9eec3a3d70f088046b7d8774f
mirsahebali <mirsahebali@protonmail.com>
2024-09-10 22:45:12 +0530

    testing foo function #3

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("./public")))
+	mux.HandleFunc("/foo", foo)
 	http.ListenAndServe(":8000", mux)
 }
 func foo(w http.ResponseWriter, r *http.Request) {
